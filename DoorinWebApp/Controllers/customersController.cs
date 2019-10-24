0790a93059a95bded661feb919ba09fa5b23e462
Johan Eriksson <joer1500@student.miun.se>
2019-10-24 10:31:48 +0200

    Lagt till modeller

---
+++ DoorinWebApp/Controllers/customersController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DoorinWebApp.Models;
+
+namespace DoorinWebApp.Controllers
+{
+    public class customersController : Controller
+    {
+        private doorinDBEntities db = new doorinDBEntities();
+
+        // GET: customers
+        public ActionResult Index()
+        {
+            return View(db.customer.ToList());
+        }
+
+        // GET: customers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            customer customer = db.customer.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // GET: customers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: customers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "customer_id,firstname,lastname,phonenumber,email,username,password,position,company")] customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.customer.Add(customer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(customer);
+        }
+
+        // GET: customers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            customer customer = db.customer.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: customers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "customer_id,firstname,lastname,phonenumber,email,username,password,position,company")] customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+
+        // GET: customers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            customer customer = db.customer.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            customer customer = db.customer.Find(id);
+            db.customer.Remove(customer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ DoorinWebApp/Controllers/freelancersController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DoorinWebApp.Models;
+
+namespace DoorinWebApp.Controllers
+{
+    public class freelancersController : Controller
+    {
+        private doorinDBEntities db = new doorinDBEntities();
+
+        // GET: freelancers
+        public ActionResult Index()
+        {
+            return View(db.freelancer.ToList());
+        }
+
+        // GET: freelancers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            freelancer freelancer = db.freelancer.Find(id);
+            if (freelancer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(freelancer);
+        }
+
+        // GET: freelancers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: freelancers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "freelancer_id,firstname,lastname,address,city,zipcode,phonenumber,email,birthdate,birthcity,nationality,username,password")] freelancer freelancer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.freelancer.Add(freelancer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(freelancer);
+        }
+
+        // GET: freelancers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            freelancer freelancer = db.freelancer.Find(id);
+            if (freelancer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(freelancer);
+        }
+
+        // POST: freelancers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "freelancer_id,firstname,lastname,address,city,zipcode,phonenumber,email,birthdate,birthcity,nationality,username,password")] freelancer freelancer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(freelancer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(freelancer);
+        }
+
+        // GET: freelancers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            freelancer freelancer = db.freelancer.Find(id);
+            if (freelancer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(freelancer);
+        }
+
+        // POST: freelancers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            freelancer freelancer = db.freelancer.Find(id);
+            db.freelancer.Remove(freelancer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
