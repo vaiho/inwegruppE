62a9ece2f8bd3d08eb9aca2d50efa0dea2314afb
Sara Vaiho <sava1401@student.miun.se>
2019-10-24 13:27:08 +0200

    Lagt till footer och header i Shared

--- DoorinWebApp/App_Start/BundleConfig.cs
+++ DoorinWebApp/App_Start/BundleConfig.cs
@@ -24,6 +24,7 @@ namespace DoorinWebApp
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/style.css",
                       "~/Content/site.css"));
         }
     }
