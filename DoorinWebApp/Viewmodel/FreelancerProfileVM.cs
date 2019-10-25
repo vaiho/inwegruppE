25227d855dd76531ddb4acd85ea9b35417702773
Johan Eriksson <joer1500@student.miun.se>
2019-10-25 16:39:57 +0200

    Testat med viewModel att joina data från olika tabeller

---
+++ DoorinWebApp/Viewmodel/FreelancerProfileVM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DoorinWebApp.Viewmodel
+{
+    public class FreelancerProfileVM
+    {
+        [Key]
+        public int Freelancer_id { get; set; }
+        [DisplayName("Förnamn")]
+        public string Firstname { get; set; }
+        [DisplayName("Efternamn")]
+        public string Lastname { get; set; }
+        [DisplayName("Epost")]
+        public string Email { get; set; }
+        [DisplayName("Nationalitet")]
+        public string Nationality { get; set; }
+        [DisplayName("Id på CV")]
+        public int Resume_id { get; set; }
+        [DisplayName("Profil")]
+        public string ProfileText { get; set; }
+
+    }
+}
