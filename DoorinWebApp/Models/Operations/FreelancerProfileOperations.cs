d36d4221fdfc89d087f09c41b96e2cf8e9aae90f
Johan Eriksson <joer1500@student.miun.se>
2019-10-28 14:52:46 +0100

    Brutit ut några funktioner till egna metoder och rensat lite kod

--- DoorinWebApp/Models/Operations/FreelancerProfileOperations.cs
+++ DoorinWebApp/Models/Operations/FreelancerProfileOperations.cs
@@ -10,25 +10,14 @@ namespace DoorinWebApp.Models.Operations
 {
     public class FreelancerProfileOperations
     {
+        
         doorinDBEntities db = new doorinDBEntities();
-        public FreelancerProfileVM GetFreelancerProfileById(int? id)
+        public FreelancerProfileVM GetFreelancerProfileById(int? id) //Metod för att hämta information om en freelancer
         {
             FreelancerProfileVM fp = new FreelancerProfileVM();
-
-            List<competence> list = new List<competence>();
-
             string sql = "SELECT freelancer.freelancer_id, firstname, lastname, resume_id, profile, email, nationality, city, birthdate from freelancer INNER JOIN resume on freelancer.freelancer_id = resume.freelancer_id WHERE freelancer.freelancer_id = @freelancer_id";
 
-            //string sql = "SELECT * FROM freelancer WHERE freelancer_id = @freelancer_id";
-
-            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-            builder.DataSource = "doorin.database.windows.net";
-            builder.UserID = "doorinadmin";
-            builder.Password = "Secretpassword123!";
-            builder.InitialCatalog = "doorinDB";
-
-
-            using (SqlConnection conn = new SqlConnection(builder.ConnectionString))
+            using (SqlConnection conn = new SqlConnection(GetBuilder().ConnectionString))
             {
                 conn.Open();
                 using (SqlCommand command = new SqlCommand(sql, conn))
@@ -52,22 +41,21 @@ namespace DoorinWebApp.Models.Operations
                     }
                 }
             }
-
-            GetCompetences(fp, builder);
-            GetTechnology(fp, builder);
+            GetCompetences(fp); //Hämtar och sparar kompetenser
+            GetTechnology(fp); //Hämtar och sparar teknologier
 
             return fp;
         }
 
-        private void GetCompetences(FreelancerProfileVM fp, SqlConnectionStringBuilder builder)
+        private void GetCompetences(FreelancerProfileVM fp)//Metod för att hämta kompetenser på inskickad freelancerVM och lagra dessa i en lista
         {
             competence c;
-            string sql2 = "SELECT competence.competence_id, name from competence_resume INNER JOIN competence on competence_resume.competence_id = competence.competence_id WHERE resume_id = @resume_id";
+            string sql = "SELECT competence.competence_id, name from competence_resume INNER JOIN competence on competence_resume.competence_id = competence.competence_id WHERE resume_id = @resume_id";
 
-            using (SqlConnection conn2 = new SqlConnection(builder.ConnectionString))
+            using (SqlConnection conn = new SqlConnection(GetBuilder().ConnectionString))
             {
-                conn2.Open();
-                using (SqlCommand command = new SqlCommand(sql2, conn2))
+                conn.Open();
+                using (SqlCommand command = new SqlCommand(sql, conn))
                 {
                     command.Parameters.AddWithValue("resume_id", fp.Resume_id);
                     using (SqlDataReader reader = command.ExecuteReader())
@@ -86,15 +74,15 @@ namespace DoorinWebApp.Models.Operations
             }
         }
 
-        private void GetTechnology(FreelancerProfileVM fp, SqlConnectionStringBuilder builder)
+        private void GetTechnology(FreelancerProfileVM fp) //Metod för att hämta teknologier på inskickad freelancerVM och lagra dessa i en lista
         {
-            string sql3 = "SELECT technology.technology_id, technology.name, technology_resume.rank, technology_resume.core_technology, technology.competence_id from technology_resume INNER JOIN technology on technology_resume.technology_id = technology.technology_id WHERE resume_id = @resume_id";
+            string sql = "SELECT technology.technology_id, technology.name, technology_resume.rank, technology_resume.core_technology, technology.competence_id from technology_resume INNER JOIN technology on technology_resume.technology_id = technology.technology_id WHERE resume_id = @resume_id";
             FullTechnology t;
 
-            using (SqlConnection conn3 = new SqlConnection(builder.ConnectionString))
+            using (SqlConnection conn = new SqlConnection(GetBuilder().ConnectionString))
             {
-                conn3.Open();
-                using (SqlCommand command = new SqlCommand(sql3, conn3))
+                conn.Open();
+                using (SqlCommand command = new SqlCommand(sql, conn))
                 {
                     command.Parameters.AddWithValue("resume_id", fp.Resume_id);
                     using (SqlDataReader reader = command.ExecuteReader())
@@ -116,5 +104,15 @@ namespace DoorinWebApp.Models.Operations
             }
         }
 
+        private SqlConnectionStringBuilder GetBuilder()
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "doorin.database.windows.net";
+            builder.UserID = "doorinadmin";
+            builder.Password = "Secretpassword123!";
+            builder.InitialCatalog = "doorinDB";
+
+            return builder;
+        }
     }    
 }
